64a976e8cd0b95a60e0f2345ee56538946d30eb4
Nrgy <cedric.burceaux@goood.pro>
2017-02-10 14:10:39 +0100

    Mise à jour du script de dép

--- deploy-prod-travis.sh
+++ deploy-prod-travis.sh
@@ -23,7 +23,16 @@ fi
 echo "### Récupération de la version actuellement en beta ###"
 git clone $GITURLBETA beta
 cd beta
-git checkout tags/$versionBeta
+git checkout -b versionBeta tags/$versionBeta
+
+if [ -d "./dist" ]
+then
+	rm -rf dist
+fi
+git clone -b gh-pages --single-branch $GITURL dist
+cd dist
+git checkout gh-pages
+# ls -a | grep -v '^\.$' | grep -v '^\.\.$' | grep -v '^\.git$' | xargs rm -rf
 mv .git ../gitdeploy
 cd ..
 npm install
